19e6c6780cd68fd394e527a6d883979ad13f5d08
Phil Ndunge <philasandendunge@gmail.com>
2021-06-18 22:39:31 +0200

    1st Commits

---
+++ Letsgo.py
@@ -0,0 +1,20 @@
+# Array Sorting, PROBLEM STATEMENT
+def minimumSwaps(arr):
+    swaps = 0
+    temp = {}
+    for i, val in enumerate(arr):
+        temp[val] = i
+    for i in range(len(arr)):
+        # because they are consecutives, I can see if the number is where it belongs
+        if arr[i] != i+1:
+            swaps += 1
+            t = arr[i]     # Variable holder
+            arr[i] = i+1
+            arr[temp[i+1]] = t
+            # Switch also the tmp array, no need to change i+1 as it's already good now
+            temp[t] = temp[i+1]
+    print(swaps)
+    return(swaps)
+
+minimumSwaps([4, 3, 1, 2])
+
