0ffdea7488529a40305701303f532d33514ff50d
Phil Ndunge <philasandendunge@gmail.com>
2021-06-25 23:30:27 +0200

    1st Commits

--- newmultiple.py
+++ newmultiple.py
@@ -1,1 +1,26 @@
-def multiple():
+def numberlinejump(x1,v1,x2,v2):
+    # 1 act, two kangaroos
+    if v2 == v1 and x2 != x1:
+        return ('NO')
+    elif x1 == x2 and v2 != v1:
+        return ('NO')
+    elif x2 > x1 and v2 > v1:
+        return ('NO')
+    elif x1 > x2 and v1 > v2:
+        return ('NO')
+    if (x1 - x2) % (v2 - v1) == 0:
+        return 'YES'
+    else:
+        return 'NO'
+    # elif x1+v1>x2+v2 and v1>v2:
+    #     return 'NO'
+    # elif x2+v2>x1+v1 and v2>v1:
+    #     return 'NO'
+    # else:
+    #     return('YES')
+    # for i in range(10000):
+    #     if x1+v1==x2+v2:
+    #         return 'YES'
+    #     x1+=v1
+    #     x2+=v2
+    # return 'NO'
